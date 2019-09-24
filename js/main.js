56f242a3e36082f9f5ee500095125208b44da7dd
Aubrey Barnes <aubrey_jones07@hotmail.com>
2019-09-24 05:55:44 -0600

    created fortune telling game

--- js/main.js
+++ js/main.js
@@ -5,101 +5,152 @@
 
 
 
-function loadguessinggameImage() {
+function loadImage() {
     
 }
 
 
 
-var companyTitle = "The Guessing Game";
+var companyTitle = "Fortune Teller";
 document.getElementById("company").innerHTML = companyTitle;
 
 
-var companySlogan = "Loops";
+var companySlogan = "Switch Statements";
 document.getElementById("slogan").innerHTML = companySlogan;
 
 
 
 
-// pick a random number between 0 and 16
+
+// pick a random number for the calendar date
+
+function getRandomIntInclusive(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max); 
+return Math.floor(Math.random() * (max - min + 1)) + min; // the maximum is inclusive and the minimum is inclusive
+
+}
+
+console.log(getRandomIntInclusive(1, 12))
+
+let month = getRandomIntInclusive(1, 12)
+let fate = getRandomIntInclusive(1, 12)
+let day = getRandomIntInclusive(1, 30)
 
 
-let correctNumber = Math.floor(Math.random() * 15)
-console.log('The correct number is: ' + correctNumber)
+function getMonthName(month) {
+let name 
+switch (month){
+case 1:
+name = "January"
+break 
+case 2: 
+name = "Februrary"
+break 
+case 3:
+name = "March"
+break 
+case 4: 
+name = "April"
+break 
+case 5:
+name = "May"
+break 
+case 6: 
+name = "June"
+break 
+case 7:
+name = "July"
+break 
+case 8: 
+name = "August"
+break 
+case 9:
+name = "September"
+break 
+case 10: 
+name = "October"
+break 
+case 11:
+name = "November"
+break 
+case 12: 
+name = "December"
+break 
+default: 
+name = "Not a month"
+break
 
 
-let guessed = false
-let totalGuesses = 0
-let gamerGuess = 0
-correctNumber += 1
+}
+return name 
+}
 
 
-function evalGuess() {
-    totalGuesses += 1 //totalGuesses = totalGuesses + 1
-    gamerGuess = document.querySelector('#guess').value
-    console.log(totalGuesses, gamerGuess)
-    const feedback = document.querySelector('#feedback')
-    
-    if (gamerGuess == correctNumber) {
-        console.log(gamerGuess + ' is equal to ' + correctNumber)
-        feedback.innerText = 'You win! ' + 'Your total guesses are ' + totalGuesses 
-        giveAward()
-    } else if (gamerGuess > correctNumber && gamerGuess < 16) {
-        feedback.innerText = 'Too high, try again'
-    } else if (gamerGuess < correctNumber && gamerGuess > 0) {
-        feedback.innerText = 'Too low, try again'
-    } else {
-        feedback.innerText = 'Please choose a number between 1 and 15 and try again' 
-        totalGuesses -= 1
-    }
-    
-    document.querySelector('#attempts').innerText = totalGuesses
-    
-    }
-        
-        
-        
-    function giveAward() {
-        console.log('Congratulations')
-        let imagePath = '#'
-    switch (totalGuesses) {
-        case 1:
-        case 2:    
-        case 3: 
-        console.log('You earned a blue ribbon!')
-        imagePath = 'images/bluefirstplaceribbon.jpg'
-        break;
-        case 4:  
-        case 5:
-        case 6:
-        console.log('You earned a red ribbon!')
-        imagePath = 'images/redsecondplaceimage.jpeg'
-        break;
-        case 7:  
-        case 8:
-        case 9:
-        case 10:  
-        case 11:
-        case 12:
-        case 13:  
-        case 14:
-        case 15:
-        console.log('You earned a yellow ribbon!')
-        imagePath = 'images/yellowthirdplaceribbon.jpg'
-        break;
-        // 
-  }
-     
-    const awardImage = document.createElement('img') // Creates a <img> element
-    awardImage.setAttribute('src', imagePath)
-    const ribbon = document.querySelector('#ribbon')
-    
-    ribbon.appendChild(awardImage)
-// only appendchild if the ribbon element does not have any child nodes  yet     
-        
-        
-    }
-    
+
+
+function getFortune(fate) {
+let message
+switch (fate){
+case 1: 
+message = 'win the lottery!'
+break 
+case 2:
+message = 'be the next greatest philanthropist!'
+break 
+case 3:
+message = 'find your soul mate!'
+break
+case 4:
+message = 'gain magical powers.'
+break 
+case 5:
+message = 'confess your darkest secrets.' 
+break 
+case 6:
+message = 'seek your greatest desire.' 
+break
+case 7:
+message = 'become the next best comedian.' 
+break 
+case 8:
+message = 'have your life turn into a musical.'  
+break 
+case 9:
+message = 'see yourself on TV.' 
+break
+case 10: 
+message = 'be seventeen again.' 
+break 
+case 11:
+message = 'have all of your hair turn gray permanently.' 
+break 
+case 12:  
+message = 'start this year all over again.'
+break
+default: 
+message = 'Some sort of error occurred.'
+break
+
+
+
+}
+
+return message
+
+
+}
+
+
+
+const monthName = getMonthName(month)
+const Fortune = getFortune(fate)
+
+const fortuneRevealed = 'Tell My Fortune: On ' + monthName + ' ' + day + ', you will ' + Fortune
+
+document.querySelector('#fortune').innerText = fortuneRevealed
+
+console.log(monthName)
     
     
 
